9629bce81aa65dc9a94ec45f131d468b0796667a
Leonard Osang <l.osang@gentics.com>
2014-02-27 13:24:36 +0100

    Implement image resizing for jpeg images.

--- scripts/import.php
+++ scripts/import.php
@@ -18,12 +18,34 @@ function generateRandomString($length = 10) {
         $randomString .= $characters[rand(0, strlen($characters) - 1)];
     }
     return $randomString;
-}	
+}
 
-if (move_uploaded_file($_FILES['userimage']['tmp_name'], $uploadfile)) {
+function make_thumb($src, $dest, $desired_width) {
+
+	/* read the source image */
+	$source_image = imagecreatefromjpeg($src);
+	$width = imagesx($source_image);
+	$height = imagesy($source_image);
+	
+	/* find the "desired height" of this thumbnail, relative to the desired width  */
+	$desired_height = floor($height * ($desired_width / $width));
+	
+	/* create a new, "virtual" image */
+	$virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+	
+	/* copy source image at a resized size */
+	imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+	
+	/* create the physical thumbnail image to its destination */
+	imagejpeg($virtual_image, $dest);
+
+	return true;
+}
+
+if (make_thumb($_FILES['userimage']['tmp_name'], $uploadfile, 1000)) {
     echo $deploymentpath . "input/" . $new_filename;
 } else {
-    echo "Möglicherweise eine Dateiupload-Attacke!\n";
+    echo "Error during processing!\n";
 }
 
 ?>
